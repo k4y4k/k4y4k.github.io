b53c4ff3dafcaa22b9f40114db5a4fd76acb4812
kayak <kayak@kayak.rocks>
2023-03-26 13:19:44 +1000

    init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,31 @@
+<main class="text-center p-8">
+  <h1 class="mb-8">
+    my name is
+    <span class="text-2xl">kayak</span>
+  </h1>
+
+  <p class="mb-8">
+    i work at
+    <a class="text-orange-500 underline text-2xl" href="http://www.acrrm.org.au"
+      >ACRRM</a
+    > (it's awesome 😀)
+  </p>
+
+  <p>problems i like to solve (no particular order):</p>
+  <ul>
+    <li>automated testing with playwright</li>
+    <li>
+      slimming down react apps so people don't need supercomputers to fill out
+      online forms
+    </li>
+    <li>R.E.M. {">"} Crowded House?</li>
+  </ul>
+
+  <h2 class="text-2xl font-bold">i am not looking for work at the moment.</h2>
+</main>
+
+<style>
+  ul li {
+    margin-bottom: 1rem;
+  }
+</style>
